d46e5441ccab0dd371417ac96967676ddc49aa14
Kamasan94 <pooldaimon94@gmail.com>
2025-03-29 17:50:00 +0100

    fe login

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import './App.css';
 
+
+
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setLoggedInUser(null);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loggedInUser ? (
+        <div>
+          <p>Welome back {loggedInUser}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <Register/>
+          <Login setLoggedInUser={setLoggedInUser}/>
+        </div>
+      )}
     </div>
   );
 }
